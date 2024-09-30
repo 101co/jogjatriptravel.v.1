cc2af4a53ad2d558605f3fa09d539588b95b79cc
antoniuspj <antoniusprabowojati@gmail.com>
2024-09-30 09:55:23 +0700

    menyelesaikan setup live

--- resources/views/livewire/main.blade.php
+++ resources/views/livewire/main.blade.php
@@ -1,4 +1,4 @@
-<div>
+<div> 
     {{-- hero --}}
     <section class="mt-16 md:mt-[80px] lg:mt-[85px] md:px-5" aria-label="JogjaTripTravel First Hero">
         <div id="indicators-carousel" class="relative w-full" data-carousel="static" data-carousel-touch="true">
@@ -7,11 +7,11 @@
                 <!-- Item 1 -->
                 <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
                     <img src="{{ asset('/storage/images/hero/jogja-5.jpg')}}"
-                        class="absolute block h-full object-cover w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        class="absolute block object-cover w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                         alt="...">
-                    <div class="absolute inset-0 bg-black bg-opacity-50 px-5 flex flex-col items-center justify-center">
+                    <div class="absolute inset-0 flex flex-col items-center justify-center px-5 bg-black bg-opacity-50">
                         <h1 class="w-4/5 mx-auto leading-8 text-center text-slate-100 text-2xl lg:text-[44px] font-bold">Pilih Tema Wisatamu Sendiri</h1>
-                        <p class="w-4/5 mx-auto leading-8 text-center text-slate-100 text-opacity-65 text-base mt-2 lg:mt-5">Temukan berbagai tema wisata sesuai dengan keinginan</p>
+                        <p class="w-4/5 mx-auto mt-2 text-base leading-8 text-center text-slate-100 text-opacity-65 lg:mt-5">Temukan berbagai tema wisata sesuai dengan keinginan</p>
                         <a href="#travel-catalogue"
                             class="z-50 text-slate-800 bg-white text-base lg:text-lg font-semibold px-8 py-2 rounded-full mt-4 lg:mt-[30px] focus:outline-none active:scale-95 transition-transform duration-150 ease-in-out">
                             Lihat Katalog
@@ -21,11 +21,11 @@
                 <!-- Item 2 -->
                 <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
                     <img src="{{ asset('/storage/images/hero/jogja-6.jpg')}}"
-                        class="absolute block h-full object-cover w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        class="absolute block object-cover w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                         alt="...">
-                    <div class="absolute inset-0 bg-black bg-opacity-50 px-5 flex flex-col items-center justify-center">
+                    <div class="absolute inset-0 flex flex-col items-center justify-center px-5 bg-black bg-opacity-50">
                         <h1 class="w-4/5 mx-auto leading-8 text-center text-slate-100 text-2xl lg:text-[44px] font-bold">Jogja Trip Travel</h1>
-                        <p class="w-3/4 mx-auto leading-1 text-center text-slate-100 text-opacity-65 text-base mt-2 lg:mt-5">Temukan informasi lebih lanjut tentang pilihan wisata dari Jogja Trip Travel</p>
+                        <p class="w-3/4 mx-auto mt-2 text-base text-center leading-1 text-slate-100 text-opacity-65 lg:mt-5">Temukan informasi lebih lanjut tentang pilihan wisata dari Jogja Trip Travel</p>
                         <a href="#contact"
                             class="z-50 text-slate-800 bg-white text-base lg:text-lg font-semibold px-8 py-2 rounded-full mt-4 lg:mt-[30px] focus:outline-none active:scale-95 transition-transform duration-150 ease-in-out">
                             Hubungi Kami
@@ -35,11 +35,11 @@
                 <!-- Item 2 -->
                 <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
                     <img src="{{ asset('/storage/images/hero/jogja-7.jpg')}}"
-                        class="absolute block h-full object-cover w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        class="absolute block object-cover w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                         alt="...">
-                    <div class="absolute inset-0 bg-black bg-opacity-50 px-5 flex flex-col items-center justify-center">
+                    <div class="absolute inset-0 flex flex-col items-center justify-center px-5 bg-black bg-opacity-50">
                         <h1 class="w-4/5 mx-auto leading-8 text-center text-slate-100 text-2xl lg:text-[44px] font-bold">Sosial Media Jogja Trip Travel</h1>
-                        <p class="w-3/4 mx-auto leading-1 text-center text-slate-100 text-opacity-65 text-base mt-2 lg:mt-5">Intip berbagai keseruan wisata Jogja melalui akun sosial media kami</p>
+                        <p class="w-3/4 mx-auto mt-2 text-base text-center leading-1 text-slate-100 text-opacity-65 lg:mt-5">Intip berbagai keseruan wisata Jogja melalui akun sosial media kami</p>
                         <a href="#contact"
                             class="z-50 text-slate-800 bg-white text-base lg:text-lg font-semibold px-8 py-2 rounded-full mt-4 lg:mt-[30px] focus:outline-none active:scale-95 transition-transform duration-150 ease-in-out">
                             Ikuti Kami
@@ -48,7 +48,7 @@
                 </div>
             </div>
             <!-- Slider indicators -->
-            <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
+            <div class="absolute z-30 flex space-x-3 -translate-x-1/2 rtl:space-x-reverse bottom-5 left-1/2">
                 <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
                     data-carousel-slide-to="0"></button>
                 <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
@@ -58,7 +58,7 @@
             </div>
             <!-- Slider controls -->
             <button type="button"
-                class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                class="absolute top-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer start-0 group focus:outline-none"
                 data-carousel-prev>
                 <span
                     class="inline-flex items-center justify-center w-6 h-6 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
@@ -71,7 +71,7 @@
                 </span>
             </button>
             <button type="button"
-                class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
+                class="absolute top-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer end-0 group focus:outline-none"
                 data-carousel-next>
                 <span
                     class="inline-flex items-center justify-center w-6 h-6 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
@@ -96,9 +96,9 @@
             <div class="relative h-[100px] lg:h-[200px] w-full md:w-[620px] lg:w-auto overflow-hidden rounded-2xl cursor-pointer hover:scale-105 active:scale-105 duration-300">
                 <a href="{{ $item->slug }}" data-carousel-item="active">
                     <img src="{{ asset('/storage/'.$item->cover_images[0])}}"
-                        class="absolute block h-full object-cover w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+                        class="absolute block object-cover w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                         alt="...">
-                    <div class="absolute inset-0 bg-black bg-opacity-50 px-5 flex flex-col items-center justify-center">
+                    <div class="absolute inset-0 flex flex-col items-center justify-center px-5 bg-black bg-opacity-50">
                         <h1 class="mx-auto text-center text-slate-100 text-xl lg:text-[26px] font-bold">{{ $item->title }}</h1>
                     </div>
                 </a>
@@ -110,9 +110,9 @@
     {{-- second hero --}}
     <section aria-label="JogjaTripTravel Second Hero" class="relative h-[191px] md:h-[275px] lg:h-[375px] overflow-hidden mt-12 md:mt-14 lg:mt-24">
         <img src="{{ asset('/storage/images/hero/jogja-10.jpg')}}"
-            class="absolute block h-full object-cover w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+            class="absolute block object-cover w-full h-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
             alt="...">
-        <div class="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center gap-2 md:gap-5">
+        <div class="absolute inset-0 flex flex-col items-center justify-center gap-2 bg-black bg-opacity-50 md:gap-5">
             <p
                 class="w-3/4 mx-auto font-light leading-1 text-center text-white opacity-95 text-opacity-65 text-2xl md:text-[30px]">
                 Enjoy Your Trip With
@@ -147,7 +147,7 @@
             <div id="accordion-collapse" data-accordion="collapse">
                 <h2 id="accordion-collapse-heading-1">
                     <button type="button"
-                        class="flex items-center justify-between w-full p-5 font-semibold text-sm rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                        class="flex items-center justify-between w-full gap-3 p-5 text-sm font-semibold text-gray-500 border border-b-0 border-gray-200 rtl:text-right rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
                         data-accordion-target="#accordion-collapse-body-1" aria-expanded="true"
                         aria-controls="accordion-collapse-body-1">
                         <span>What is Jogja Trip Travel?</span>
@@ -160,12 +160,12 @@
                 </h2>
                 <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
                     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                        <p class="mb-2 text-gray-500 dark:text-gray-400 font-light text-sm">Jogja Trip Travel is a tourism service bureau domiciled in the city of Yogyakarta. Providing various tour packages that local and foreign tourists can choose from.</p>
+                        <p class="mb-2 text-sm font-light text-gray-500 dark:text-gray-400">Jogja Trip Travel is a tourism service bureau domiciled in the city of Yogyakarta. Providing various tour packages that local and foreign tourists can choose from.</p>
                     </div>
                 </div>
                 <h2 id="accordion-collapse-heading-2">
                     <button type="button"
-                        class="flex items-center justify-between w-full p-5 font-semibold text-sm rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                        class="flex items-center justify-between w-full gap-3 p-5 text-sm font-semibold text-gray-500 border border-b-0 border-gray-200 rtl:text-right focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
                         data-accordion-target="#accordion-collapse-body-2" aria-expanded="true"
                         aria-controls="accordion-collapse-body-1">
                         <span>How to order toursim package?</span>
@@ -178,12 +178,12 @@
                 </h2>
                 <div id="accordion-collapse-body-2" class="hidden" aria-labelledby="accordion-collapse-heading-2">
                     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                        <p class="mb-2 text-gray-500 dark:text-gray-400 font-light text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem at perspiciatis in eligendi. Nam soluta veritatis accusantium rerum adipisci, inventore nostrum distinctio cupiditate laborum? Excepturi quibusdam magni quaerat minus placeat..</p>
+                        <p class="mb-2 text-sm font-light text-gray-500 dark:text-gray-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem at perspiciatis in eligendi. Nam soluta veritatis accusantium rerum adipisci, inventore nostrum distinctio cupiditate laborum? Excepturi quibusdam magni quaerat minus placeat..</p>
                     </div>
                 </div>
                 <h2 id="accordion-collapse-heading-3">
                     <button type="button"
-                        class="flex items-center justify-between w-full p-5 font-semibold text-sm rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3"
+                        class="flex items-center justify-between w-full gap-3 p-5 text-sm font-semibold text-gray-500 border border-gray-200 rtl:text-right focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
                         data-accordion-target="#accordion-collapse-body-3" aria-expanded="false"
                         aria-controls="accordion-collapse-body-3">
                         <span class="text-start">Can we make a Down Payment?</span>
@@ -196,7 +196,7 @@
                 </h2>
                 <div id="accordion-collapse-body-3" class="hidden" aria-labelledby="accordion-collapse-heading-3">
                     <div class="p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900">
-                        <p class="mb-2 text-gray-500 dark:text-gray-400 font-light text-sm">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, rerum alias velit minima tempora nostrum similique esse facilis explicabo provident ut exercitationem enim consectetur necessitatibus totam nulla quibusdam quam quasi!</p>
+                        <p class="mb-2 text-sm font-light text-gray-500 dark:text-gray-400">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et, rerum alias velit minima tempora nostrum similique esse facilis explicabo provident ut exercitationem enim consectetur necessitatibus totam nulla quibusdam quam quasi!</p>
                     </div>
                 </div>
             </div>
